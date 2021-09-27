4da994c65ee61413a5836878402734f884cb0905
Atilla <you@example.com>
2021-09-27 13:46:35 +0200

    yo pog now buisnesess can also buy ice cream, good thing the code was quite easy to make

--- PapiGelato.py
+++ PapiGelato.py
@@ -12,6 +12,7 @@ def toppings():
         print("Topping          1 x €"+ str(toppingkosten) +"    = €"+ str(toppingkosten))
     else:
         pass
+    
 def bonnetjeeinde():
     print("                              ------ +")
 
@@ -30,129 +31,177 @@ toppingkosten = 0
 
 print("Welkom bij Papi Gelato.")
 print("")
+
+while (True):
+    parofza = input("Bent u 1) particulier of 2) zakelijk? ")
+    if (parofza == "1" or parofza == "2"):
+        break
+    else:
+        imsorry()
+        continue
+
 while (end == False):
     hoeveelheid = ("")
     aantalvraag = ("")
     x = 1
     counter = 0
 
-    while (True):
-        hoeveelheid = input("Hoeveel bolletjes wilt u? ")
-        if (int(hoeveelheid.isnumeric()) == False):
-            imsorry()
-            continue
-        elif (int(hoeveelheid) > 8):
-            print("Sorry, zulke grote bakken hebben we niet")
-            continue
-        elif (int(hoeveelheid) == 1 or int(hoeveelheid) == 2 or int(hoeveelheid) == 3):
-            break
-        elif (int(hoeveelheid) > 3 and int(hoeveelheid) < 9):
-            print("Dan krijgt u van mij een bakje met "+ str(hoeveelheid) +" bolletjes")
-            keuze = ("bakje")
-            heeftbakjebesteld = True
-            hoeveelheidbakje = int(hoeveelheidbakje) + 1
-            bakjecounter = bakjecounter + 1
-            break
-        elif (int(hoeveelheid.isnumeric()) == True):
-            imsorry()
-            continue
-    if (hoeveelheidoplsaan == 0):
-        hoeveelheidoplsaan = 0 + int(hoeveelheid)
-    else:
-        hoeveelheidoplsaan = int(hoeveelheidoplsaan) + int(hoeveelheid)
 
-    if (int(hoeveelheid) > 3 and int(hoeveelheid) < 9):
-       print("")
-    else:
-         while (True):
-            aantalvraag = input("Wilt u deze "+ str(hoeveelheid) +" bolletje(s) in A) een hoorntje of B) een bakje? (Hoorn/Bakje) ")
-            if (aantalvraag.lower() == "hoorn" ):
-             keuze = ("hoorn")
-             heefthornbesteld = True
-             hoeveelheidhoorn =  int(hoeveelheidhoorn) + 1
-             hoorncounter = hoorncounter + 1
-             break
-            elif (aantalvraag.lower() == "bakje"):
-             keuze = ("bakje")
-             heeftbakjebesteld = True
-             hoeveelheidbakje = int(hoeveelheidbakje) + 1
-             bakjecounter = bakjecounter + 1
-             break
+
+    if (parofza == "2"):
+        while (True):
+            lhoeveelheid = input("Hoeveel Liter wilt u? ")
+            if (lhoeveelheid == ValueError or int(lhoeveelheid) < 1):
+                imsorry()
+                continue
             else:
-             print("Sorry dat snap ik niet...")
-             continue
-
-    while (True):
-        whattopping = input("Wat voor topping wilt u: A) Geen, B) Slagroom, C) Sprinkels of D) Caramel Saus? ")
-        if (whattopping.lower() == "b" or whattopping.lower() == "c" or whattopping.lower() == "d" or whattopping.lower() == "a"):
-            break
-        else:
-            print("Sorry dat snap ik niet...")
-            continue
-
-    
-    while (int(hoeveelheid) > int(counter)):
-        bolsmaak = input("Welke smaak wilt u voor bolletje nummer "+ str(counter + 1) +"? A) Aardbei, C) Chocolade, M) Munt of V) Vanille? ")
-        if  (int(hoeveelheid) < int(counter)):
-            break
-
-        elif (bolsmaak.lower() == "a" or bolsmaak.lower() == "c" or bolsmaak.lower() == "m" or bolsmaak.lower() == "v"):
-            counter = int(counter) + 1
-            continue
-        elif (bolsmaak.lower() != "a" or bolsmaak.lower() != "c" or bolsmaak.lower() != "m" or bolsmaak.lower() !=  "v"):
-            imsorry()
-            continue
-    
-    if (whattopping.lower() == "b"):
-        toppingkosten = float(toppingkosten) + 0.50
-    elif (whattopping.lower() == "c"):
-        toppingkosten = float(toppingkosten) + float(hoeveelheid) * 0.30
-    elif (whattopping.lower() == "d"):
-        toppingkosten = float(toppingkosten) + float(hoeveelheid) + (float(hoeveelheidbakje) * 0.90 + float(hoeveelheidhoorn) + 0.60)
-
-    while (True):
-        nogeenkeer = input("Hier is uw "+ str(keuze) +" met "+ str(hoeveelheid) +" bolletje(s). Wilt u nog meer bestellen? (Y/N) ")
-        if (nogeenkeer.lower() == "y"):
-            break
-        elif(nogeenkeer.lower() == "n"):
-            end = True
-            break
-        else:
-            imsorry()
-            continue
-
-
-    if (end == True):
+                break
+        
+        while (int(lhoeveelheid) > int(counter)):
+            bolsmaak = input("Welke smaak wilt u voor liter nummer "+ str(counter + 1) +"? A) Aardbei, C) Chocolade, M) Munt of V) Vanille? ")
+            if  (int(lhoeveelheid) < int(counter)):
+                break
+            elif (bolsmaak.lower() == "a" or bolsmaak.lower() == "c" or bolsmaak.lower() == "m" or bolsmaak.lower() == "v"):
+                counter = int(counter) + 1
+                continue
+            elif (bolsmaak.lower() != "a" or bolsmaak.lower() != "c" or bolsmaak.lower() != "m" or bolsmaak.lower() !=  "v"):
+                imsorry()
+                continue
         break
-    else:
-        continue
+        
+        
+
+
+    elif (parofza == "1"):
+        while (True):
+            hoeveelheid = input("Hoeveel bolletjes wilt u? ")
+            if (int(hoeveelheid.isnumeric()) == False):
+                imsorry()
+                continue
+            elif (int(hoeveelheid) > 8):
+                print("Sorry, zulke grote bakken hebben we niet")
+                continue
+            elif (int(hoeveelheid) == 1 or int(hoeveelheid) == 2 or int(hoeveelheid) == 3):
+                break
+            elif (int(hoeveelheid) > 3 and int(hoeveelheid) < 9):
+                print("Dan krijgt u van mij een bakje met "+ str(hoeveelheid) +" bolletjes")
+                keuze = ("bakje")
+                heeftbakjebesteld = True
+                hoeveelheidbakje = int(hoeveelheidbakje) + 1
+                bakjecounter = bakjecounter + 1
+                break
+            elif (int(hoeveelheid.isnumeric()) == True):
+                imsorry()
+                continue
+        if (hoeveelheidoplsaan == 0):
+            hoeveelheidoplsaan = 0 + int(hoeveelheid)
+        else:
+            hoeveelheidoplsaan = int(hoeveelheidoplsaan) + int(hoeveelheid)
+
+        if (int(hoeveelheid) > 3 and int(hoeveelheid) < 9):
+         print("")
+        else:
+            while (True):
+                aantalvraag = input("Wilt u deze "+ str(hoeveelheid) +" bolletje(s) in A) een hoorntje of B) een bakje? ")
+                if (aantalvraag.lower() == "hoorn" ):
+                    keuze = ("a")
+                    heefthornbesteld = True
+                    hoeveelheidhoorn =  int(hoeveelheidhoorn) + 1
+                    hoorncounter = hoorncounter + 1
+                    break
+                elif (aantalvraag.lower() == "bakje"):
+                    keuze = ("b")
+                    heeftbakjebesteld = True
+                    hoeveelheidbakje = int(hoeveelheidbakje) + 1
+                    bakjecounter = bakjecounter + 1
+                    break
+                else:
+                    imsorry()
+                    continue
+
+        while (True):
+            whattopping = input("Wat voor topping wilt u: A) Geen, B) Slagroom, C) Sprinkels of D) Caramel Saus? ")
+            if (whattopping.lower() == "b" or whattopping.lower() == "c" or whattopping.lower() == "d" or whattopping.lower() == "a"):
+                break
+            else:
+                imsorry()
+                continue
+
+        
+        while (int(hoeveelheid) > int(counter)):
+            bolsmaak = input("Welke smaak wilt u voor bolletje nummer "+ str(counter + 1) +"? A) Aardbei, C) Chocolade, M) Munt of V) Vanille? ")
+            if  (int(hoeveelheid) < int(counter)):
+                break
+
+            elif (bolsmaak.lower() == "a" or bolsmaak.lower() == "c" or bolsmaak.lower() == "m" or bolsmaak.lower() == "v"):
+                counter = int(counter) + 1
+                continue
+            elif (bolsmaak.lower() != "a" or bolsmaak.lower() != "c" or bolsmaak.lower() != "m" or bolsmaak.lower() !=  "v"):
+                imsorry()
+                continue
+        
+        if (whattopping.lower() == "b"):
+            toppingkosten = float(toppingkosten) + 0.50
+        elif (whattopping.lower() == "c"):
+            toppingkosten = float(toppingkosten) + float(hoeveelheid) * 0.30
+        elif (whattopping.lower() == "d"):
+            toppingkosten = float(toppingkosten) + float(hoeveelheid) + (float(hoeveelheidbakje) * 0.90 + float(hoeveelheidhoorn) + 0.60)
+
+        while (True):
+            nogeenkeer = input("Hier is uw "+ str(keuze) +" met "+ str(hoeveelheid) +" bolletje(s). Wilt u nog meer bestellen? (Y/N) ")
+            if (nogeenkeer.lower() == "y"):
+                break
+            elif(nogeenkeer.lower() == "n"):
+                end = True
+                break
+            else:
+                imsorry()
+                continue
+
+
+        if (end == True):
+            break
+        else:
+            continue
+    
 
 
 
+if (parofza == "1"):
+    hoornkosten = (float(hoorncounter * 1.25))
+    bakjekosten = (float(bakjecounter) * 0.75)
+    bolkost = (float(hoeveelheidoplsaan) * 1.10)
+    totaaluitrekeningh = (float(hoeveelheidoplsaan) * 1.10 + float(hoornkosten) + float(toppingkosten))
+    totaaluitrekeningb = (float(hoeveelheidoplsaan) * 1.10 + float(bakjekosten) + float(toppingkosten))
+    totaaluitrekeningbeide = (float(hoeveelheidoplsaan) * 1.10 + float(bakjekosten) + float(hoornkosten) + float(toppingkosten))
 
-hoornkosten = (float(hoorncounter * 1.25))
-bakjekosten = (float(bakjecounter) * 0.75)
-bolkost = (float(hoeveelheidoplsaan) * 1.10)
-totaaluitrekeningh = (float(hoeveelheidoplsaan) * 1.10 + float(hoornkosten) + float(toppingkosten))
-totaaluitrekeningb = (float(hoeveelheidoplsaan) * 1.10 + float(bakjekosten) + float(toppingkosten))
-totaaluitrekeningbeide = (float(hoeveelheidoplsaan) * 1.10 + float(bakjekosten) + float(hoornkosten) + float(toppingkosten))
+    print("------------['Papi gelato']------------")
+    print("")
+    print("Bolletjes        "+ str(hoeveelheidoplsaan) +" x €1.10   = €"+ str(round(bolkost, 2)))
+    if (heefthornbesteld == True and heeftbakjebesteld == True):
+        hoorncalc()
+        bakjecalc()
+        toppings()
+        bonnetjeeinde()
+        print("Totaal                       = €"+ str(round(totaaluitrekeningbeide, 2)))
+    elif (heefthornbesteld == True):
+        hoorncalc()
+        toppings()
+        bonnetjeeinde()
+        print("Totaal                       = €"+ str(round(totaaluitrekeningh, 2)))
+    elif (heeftbakjebesteld == True):
+        bakjecalc()
+        toppings()
+        bonnetjeeinde()
+        print("Totaal                       = €"+ str(round(totaaluitrekeningb, 2)))
 
-print("------------['Papi gelato']------------")
-print("")
-print("Bolletjes        "+ str(hoeveelheidoplsaan) +" x €1.10   = €"+ str(round(bolkost, 2)))
-if (heefthornbesteld == True and heeftbakjebesteld == True):
-    hoorncalc()
-    bakjecalc()
-    toppings()
+elif (parofza == "2"):
+    toltaaluitrekenenliter = (float(lhoeveelheid) * 9.80)
+    btwuitrekenen = (toltaaluitrekenenliter / 100) * 9
+    print("------------['Papi gelato']------------")
+    print("")
+
+    print("Liter      "+ str(lhoeveelheid) +" x €9.80         = €"+ str(round(toltaaluitrekenenliter, 2)))
     bonnetjeeinde()
-    print("Totaal                       = €"+ str(round(totaaluitrekeningbeide, 2)))
-elif (heefthornbesteld == True):
-    hoorncalc()
-    toppings()
-    bonnetjeeinde()
-    print("Totaal                       = €"+ str(round(totaaluitrekeningh, 2)))
-elif (heeftbakjebesteld == True):
-    bakjecalc()
-    toppings()
-    bonnetjeeinde()
-    print("Totaal                       = €"+ str(round(totaaluitrekeningb, 2)))
+    print("Totaal                       = €"+ str(round(toltaaluitrekenenliter, 2)))
+    print("BTW (9%)                     = €"+ str(round(btwuitrekenen,2 )))
